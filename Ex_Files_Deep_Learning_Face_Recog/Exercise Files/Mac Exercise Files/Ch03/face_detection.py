2ea2bff29eb07c87beb8a90c195a62fd4a1f6bb2
Ricardo Leão Soares <ricsleao@gmail.com>
2020-12-09 15:32:24 -0300

    chap 3 - face_recognition

--- Ex_Files_Deep_Learning_Face_Recog/Exercise Files/Mac Exercise Files/Ch03/face_detection.py
+++ Ex_Files_Deep_Learning_Face_Recog/Exercise Files/Mac Exercise Files/Ch03/face_detection.py
@@ -1,12 +1,14 @@
 import PIL.Image
 import PIL.ImageDraw
-
+# PIL => Python Image Library
+import face_recognition
 
 # Load the jpg file into a numpy array
-
+image = face_recognition.load_image_file("people.jpg")
 
 # Find all the faces in the image
-
+face_locations = face_recognition.face_locations(image, 1, "hog")
+#face_locations = face_recognition.face_locations(image, 1, "cnn")
 
 number_of_faces = len(face_locations)
 print("I found {} face(s) in this photograph.".format(number_of_faces))
@@ -17,6 +19,8 @@ pil_image = PIL.Image.fromarray(image)
 for face_location in face_locations:
 
     # Print the location of each face in this image. Each face is a list of co-ordinates in (top, right, bottom, left) order.
+    top, right, bottom, left = face_location
+
 
     print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(top, left, bottom, right))
 
